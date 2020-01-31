8a19e13f205b3623fe693dcc1667084b65d8f5e6
Yanet Fonseca <yfonsecalopez@gmail.com>
2020-01-31 00:05:47 -0500

    first commit

---
+++ Dog/Dog.cs
@@ -0,0 +1,118 @@
+﻿/**
+ * This class models a Dog.
+ * @author Rob Kelley
+ * @author Elena Milan Lopez
+ * @version 1.0
+ * Project 1 -Solution
+ * SP20
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dog
+{
+	public class Dog
+	{
+
+		private int age;
+		private String name;
+
+		/**
+		 * Empty-argument constructor to put object
+		 * into a consistent state.
+		 */
+		public Dog()
+		{
+			age = 0;
+			name = "";
+		}//end constructor
+
+		/**
+		 * Constructor accepts the age and
+		 * name of the dog. This is the preferred
+		 * constructor.
+		 * @param age
+		 * @param name
+		 */
+		public Dog(int age, String name)
+		{
+
+			this.setAge(age);
+			this.setName(name);
+
+		}//end constructor
+
+		public int calcDogYears()
+		{
+			int dogYears = 15 + (this.age - 1) * 7;
+			return dogYears;
+		}
+
+		/**
+		 * Getter for age
+		 * @return
+		 */
+		public int getAge()
+		{
+			return age;
+		}//end getAge
+
+		/**
+		 * Setter for age
+		 * Checks the bounds so 
+		 * @param age
+		 */
+		public void setAge(int age)
+		{
+			if (age < 0)
+				this.age = 0;
+			else
+				this.age = age;
+		}//end setAge
+
+		/**
+		 * Getter for name
+		 * @return
+		 */
+		public String getName()
+		{
+			return name;
+		}//end getName
+
+		/**
+		 * Setter for name. Ensures that only
+		 * alpha characters are allowed in the name.
+		 * @param name
+		 */
+		public void setName(String name)
+		{
+			bool valid = true;
+			char[] n = name.ToCharArray();
+			
+			for (int i = 0; i < n.Length; i++)
+			{
+				if (!char.IsLetter(n[i]))
+				{
+					valid = false;
+					break;
+				}
+			}
+
+			if (valid)
+				this.name = name;
+			else
+				this.name = "";
+
+		}//end setName
+
+	     public String toString()
+		{
+			return "Dog [age=" + age + ", name=" + name + "]";
+			
+		}//end toString
+
+	}//end class
+}
+
+
---
+++ Dog/DogTest.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * This class tests the Dog class.
+ * @author Elena Milan Lopez
+ * @version 1.0
+ * Project 1 -Solution
+ * SP20
+ */
+
+using System;
+
+namespace Dog
+{
+     class DogTest
+     {
+          static void Main(string[] args)
+          {
+
+               Dog dog1 = new Dog();
+               Dog dog2 = new Dog(5,"Tommy");
+             
+               
+               
+               dog1.setAge(7);
+               dog1.setName("Pepe");
+             
+               Console.WriteLine("Dog1 name is: " + dog1.getName() + " and Dog2  name is:" + dog2.getName());
+               Console.WriteLine("Dog1 age is: " + dog1.getAge()  + " and Dog2  age is:" + dog2.getAge());
+               
+               Console.WriteLine("Dog1 is: " + dog1.calcDogYears() + " years old.");
+               Console.WriteLine("Dog2 is: " + dog2.calcDogYears() + " years old.");
+               
+               Console.WriteLine(dog1.toString());
+               Console.WriteLine(dog2.toString());
+         
+          }
+     }
+}
